d08c65ec66ccd9a4bdc5faf17e5df114cc913a9b
Marek Starzyk <stachu.ms9@gmail.com>
2020-12-16 23:10:17 +0100

    add homepade.html, idex.html. and main.sh

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+    movies = []
+    for item in range(1, 10):
+        movies.append('film')
+
+    return render_template('homepage.html', movies=movies)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
