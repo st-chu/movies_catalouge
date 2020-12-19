e14188ee8cd34a4ee2dd947162ac053320a333a1
Marek Starzyk <stachu.ms9@gmail.com>
2020-12-19 01:45:34 +0100

    Add movie detalist, get_single_movie, get_single_movie_cast and get_movie_image

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 import tmdb_client
-from tmdb_client import popular_movies
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def homepage():
-    movie = tmdb_client.popular_movies(12)
+    movie = tmdb_client.popular_movies(8)
     return render_template('homepage.html', movies=movie)
 
 
@@ -16,6 +16,14 @@ def utility_processor():
     def tmdb_image_url(path, size):
         return tmdb_client.image_url(path, size)
     return {"tmdb_image_url": tmdb_image_url}
+
+
+@app.route("/movie/<int:movie_id>")
+def movie_details(movie_id):
+    details = tmdb_client.get_single_movie(movie_id)
+    cast = tmdb_client.get_single_movie_cast(movie_id)
+    image = tmdb_client.get_movie_image(movie_id)
+    return render_template("movie_details.html", movie=details, cast=cast, image_path=image)
 
 
 if __name__ == '__main__':
--- tmdb_client.py
+++ tmdb_client.py
@@ -1,11 +1,14 @@
 import requests
+import random
+
+
+token = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZDVkOWRlZTdjMzEwMjlkZWQ2NDBkYzE3NTEwNTg3YSIsInN1YiI6IjVmZDhmN' \
+            '2NjMWYzZTYwMDAzZmIyOTJiMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ewqO0KDgM6YtYjqzvQNBrZ-xgdk' \
+            'A7nl1DBn5hO9XnL0'
 
 
 def popular_movies(how_many):
     endpoint = 'https://api.themoviedb.org/3/movie/popular'
-    token = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZDVkOWRlZTdjMzEwMjlkZWQ2NDBkYzE3NTEwNTg3YSIsInN1YiI6IjVmZDhmN' \
-            '2NjMWYzZTYwMDAzZmIyOTJiMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ewqO0KDgM6YtYjqzvQNBrZ-xgdk' \
-            'A7nl1DBn5hO9XnL0'
     headers = {
         'language': 'pl',
         'Authorization': token,
@@ -13,8 +16,7 @@ def popular_movies(how_many):
         'languages': 'pl-PL'
     }
     r = requests.get(url=endpoint, headers=headers)
-
-    return r.json().get('results')[:how_many]
+    return [random.choice(r.json().get('results')) for i in range(how_many)]
 
 
 def image_url(file_path, file_size="w342"):
@@ -22,4 +24,23 @@ def image_url(file_path, file_size="w342"):
     return f'{base_url}{file_size}{file_path}'
 
 
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {'Authorization': token}
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+
+def get_single_movie_cast(movie_id):
+    endpoint = f'https://api.themoviedb.org/3/movie/{movie_id}/credits'
+    headers = {'Authorization': token}
+    response = requests.get(endpoint, headers=headers)
+    return response.json()['cast']
+
+
+def get_movie_image(movie_id):
+    endpoint = f'https://api.themoviedb.org/3/movie/{movie_id}/images'
+    headers = {'Authorization': token}
+    response = requests.get(endpoint, headers=headers)
+    return random.choice(response.json()['backdrops'])
 
