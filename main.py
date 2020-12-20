7812bc57af4429d66f6b9e8ac9cc120e77aea3f1
Marek Starzyk <stachu.ms9@gmail.com>
2020-12-20 18:28:29 +0100

    add dynamic HTML structure of movie list selection buttons, active list indicator and validation of the selected list type

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import tmdb_client
-
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def homepage():
-    movie = tmdb_client.popular_movies(8)
-    return render_template('homepage.html', movies=movie)
+    movies_list = ['popular', 'now_playing', 'upcoming', 'top_rated', 'niepoprawna_wartość']
+    selected_list = request.args.get('list_type', 'popular')
+    try:
+        movie = tmdb_client.get_movies(8, selected_list)
+        return render_template('homepage.html', movies=movie, movies_list=movies_list, selected_list=selected_list)
+    except:
+        movie = tmdb_client.get_movies(8, 'popular')
+        return render_template('homepage.html', movies=movie, movies_list=movies_list, selected_list='popular')
 
 
 @app.context_processor
 def utility_processor():
     def tmdb_image_url(path, size):
         return tmdb_client.image_url(path, size)
+
     return {"tmdb_image_url": tmdb_image_url}
 
 
--- tmdb_client.py
+++ tmdb_client.py
@@ -1,14 +1,13 @@
 import requests
 import random
+from typing import List, Dict, Union
 
 
-token = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZDVkOWRlZTdjMzEwMjlkZWQ2NDBkYzE3NTEwNTg3YSIsInN1YiI6IjVmZDhmN' \
-            '2NjMWYzZTYwMDAzZmIyOTJiMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ewqO0KDgM6YtYjqzvQNBrZ-xgdk' \
-            'A7nl1DBn5hO9XnL0'
+token = '*********'
 
 
-def popular_movies(how_many):
-    endpoint = 'https://api.themoviedb.org/3/movie/popular'
+def get_movies_list(list_name: str) -> Dict[str, Union[int, List[Dict[str, Union[str, bool, int, float, List[int]]]]]]:
+    endpoint = f'https://api.themoviedb.org/3/movie/{list_name}'
     headers = {
         'language': 'pl',
         'Authorization': token,
@@ -16,29 +15,39 @@ def popular_movies(how_many):
         'languages': 'pl-PL'
     }
     r = requests.get(url=endpoint, headers=headers)
-    return [random.choice(r.json().get('results')) for i in range(how_many)]
+    r.raise_for_status()
+    return r.json()
 
 
-def image_url(file_path, file_size="w342"):
+def get_movies(how_many: int, list_name: str) -> List[Dict[str, Union[bool, str, List[int], int, float]]]:
+    movies = get_movies_list(list_name)
+    return movies['results'][:how_many]
+
+
+def image_url(file_path: str, file_size="w342") -> str:
     base_url = "http://image.tmdb.org/t/p/"
     return f'{base_url}{file_size}{file_path}'
 
 
-def get_single_movie(movie_id):
+def get_single_movie(
+        movie_id: int
+) -> Dict[str, Union[bool, str, Dict[str, Union[int, str]], int, List[Dict[str, Union[int, str]]], float,
+                     List[Dict[str, str]]]]:
     endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
     headers = {'Authorization': token}
     response = requests.get(endpoint, headers=headers)
     return response.json()
 
 
-def get_single_movie_cast(movie_id):
+def get_single_movie_cast(movie_id: int):
     endpoint = f'https://api.themoviedb.org/3/movie/{movie_id}/credits'
     headers = {'Authorization': token}
     response = requests.get(endpoint, headers=headers)
+    print(response.json()['cast'])
     return response.json()['cast']
 
 
-def get_movie_image(movie_id):
+def get_movie_image(movie_id: int) -> List[Dict[str, Union[bool, int, str, float]]]:
     endpoint = f'https://api.themoviedb.org/3/movie/{movie_id}/images'
     headers = {'Authorization': token}
     response = requests.get(endpoint, headers=headers)
