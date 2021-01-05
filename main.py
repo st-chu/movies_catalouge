dd3f3411d3a623c6f719d0b2192ec24313fc8149
Marek Starzyk <stachu.ms9@gmail.com>
2021-01-05 22:22:49 +0100

    to check

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import tmdb_client
+import datetime
+
+FAVORITES = set([])
 
 app = Flask(__name__)
 
@@ -8,6 +11,7 @@ app = Flask(__name__)
 def homepage():
     movies_list = ['popular', 'now_playing', 'upcoming', 'top_rated', 'niepoprawna_wartość']
     selected_list = request.args.get('list_type', 'popular')
+    print(FAVORITES)
     try:
         movie = tmdb_client.get_movies(8, selected_list)
         return render_template('homepage.html', movies=movie, movies_list=movies_list, selected_list=selected_list)
@@ -30,6 +34,37 @@ def movie_details(movie_id):
     cast = tmdb_client.get_single_movie_cast(movie_id)
     image = tmdb_client.get_movie_image(movie_id)
     return render_template("movie_details.html", movie=details, cast=cast, image_path=image)
+
+
+@app.route("/search")
+def search():
+    search_query = request.args.get('q', '')
+    if search_query:
+        movies = tmdb_client.search(search_query=search_query)
+    else:
+        movies = []
+    return render_template('search.html', movies=movies, search_query=search_query)
+
+
+@app.route("/tv-today")
+def tv_today():
+    date = datetime.date.today()
+    try:
+        tv = tmdb_client.get_tv_airing_today()
+        return render_template('tv_today.html', tv=tv, date=date)
+    except:
+        tv = []
+        return render_template('tv_today.html', tv=tv, date=date)
+
+
+@app.route("/favorites/add", methods=['POST'])
+def add_to_favourites():
+    movie_id = request.form.get('movie_id')
+    selected_list = request.args.get('list_type') tu
+    print(selected_list)
+    if movie_id:
+        FAVORITES.add(movie_id)
+    return redirect(url_for('homepage', selected_list=selected_list)) tu
 
 
 if __name__ == '__main__':
--- tmdb_client.py
+++ tmdb_client.py
@@ -3,16 +3,18 @@ import random
 from typing import List, Dict, Union
 
 
-token = '*********'
+token = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZDVkOWRlZTdjMzEwMjlkZWQ2NDBkYzE3NTEwNTg3YSIsInN1YiI6IjVmZDhmN' \
+            '2NjMWYzZTYwMDAzZmIyOTJiMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ewqO0KDgM6YtYjqzvQNBrZ-xgdk' \
+            'A7nl1DBn5hO9XnL0'
+
+
+base_url = 'https://api.themoviedb.org/3/'
 
 
 def get_movies_list(list_name: str) -> Dict[str, Union[int, List[Dict[str, Union[str, bool, int, float, List[int]]]]]]:
-    endpoint = f'https://api.themoviedb.org/3/movie/{list_name}'
+    endpoint = f'https://api.themoviedb.org/3/movie/{list_name}?language=pl-PL'
     headers = {
-        'language': 'pl',
         'Authorization': token,
-        'Content-Type': 'application/json;charset=utf-8',
-        'languages': 'pl-PL'
     }
     r = requests.get(url=endpoint, headers=headers)
     r.raise_for_status()
@@ -33,7 +35,7 @@ def get_single_movie(
         movie_id: int
 ) -> Dict[str, Union[bool, str, Dict[str, Union[int, str]], int, List[Dict[str, Union[int, str]]], float,
                      List[Dict[str, str]]]]:
-    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}?language=pl-PL"
     headers = {'Authorization': token}
     response = requests.get(endpoint, headers=headers)
     return response.json()
@@ -43,7 +45,6 @@ def get_single_movie_cast(movie_id: int):
     endpoint = f'https://api.themoviedb.org/3/movie/{movie_id}/credits'
     headers = {'Authorization': token}
     response = requests.get(endpoint, headers=headers)
-    print(response.json()['cast'])
     return response.json()['cast']
 
 
@@ -52,4 +53,21 @@ def get_movie_image(movie_id: int) -> List[Dict[str, Union[bool, int, str, float
     headers = {'Authorization': token}
     response = requests.get(endpoint, headers=headers)
     return random.choice(response.json()['backdrops'])
+
+
+def search(search_query: str):
+    endpoint = f'https://api.themoviedb.org/3/search/movie?query={search_query}'
+    headers = {'Authorization': token}
+    response = requests.get(endpoint, headers=headers)
+    return response.json()['results']
+
+
+def get_tv_airing_today():
+    endpoint = f'{base_url}tv/airing_today?language=pl-PL'
+    headers = {'Authorization': token}
+    response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
+    return response.json()['results']
+
+
 
